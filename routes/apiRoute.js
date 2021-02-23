568fcadafb5f7cf6caad8ee6d5e97b6841721ad4
Nehreet Kaur <kaur.nehreet@gmail.com>
2021-02-23 00:40:26 -0600

    add limit option in apiRoute

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -9,28 +9,31 @@ module.exports = function (app) {
             res.json(data);
         })
 
-    });
 
-app.post("/api/workouts", (req, res) => {
-    db.Workout.create({}
-        , (err, data) => {
+    });
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create({}
+            , (err, data) => {
+                if (err) return err;
+                else res.json(data);
+            })
+    });
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, (err, data) => {
             if (err) return err;
             else res.json(data);
         })
-});
-app.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, (err, data) => {
-        if (err) return err;
-        else res.json(data);
+    });
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.aggregate([{
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }, { $limit: 7 }
+        ]).then((data) => {
+            res.json(data);
+        });
     })
-});
-app.get("/api/workouts/range", (req, res) => {
-    db.Workout.aggregate([{
-        $addFields: {
-            totalDuration: { $sum: "$exercises.duration" }
-        }
-    }]).then((data) => {
-        res.json(data);
-    })
-});
 };
+
+
